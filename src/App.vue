76027859bd326e67398ea6d1531a132cb80c2b45
Adam Antal <adoantal@gmail.com>
2026-04-12 17:48:21 +0200

    Initial TW setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div class="flex h-screen items-center justify-center bg-gray-900">
+    <h1 class="text-4xl font-bold text-green-400">
+      Pokémon Speed App ⚡
+    </h1>
+
+    <p class="mt-4 text-white">
+      Count: {{ count }}
+    </p>
+
+    <button
+      class="mt-4 rounded bg-blue-500 px-4 py-2 text-white"
+      @click="increment"
+    >
+      Increment
+    </button>
+  </div>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+
+  methods: {
+    increment() {
+      this.count++
+    },
+  },
+}
+</script>
