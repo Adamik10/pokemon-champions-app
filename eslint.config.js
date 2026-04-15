960aeded0b3b3c10b3e286fb7509fec571187d39
Adam Antal <adoantal@gmail.com>
2026-04-15 17:11:42 +0200

    Make TS + Eslint + prettier work together

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js"
+import vue from "eslint-plugin-vue"
+import tseslint from "typescript-eslint"
+import vueParser from "vue-eslint-parser"
+
+export default [
+  // Base JS rules
+  js.configs.recommended,
+
+  // TypeScript rules
+  ...tseslint.configs.recommended,
+
+  // Vue rules
+  ...vue.configs["flat/recommended"],
+
+  //  Vue files
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+
+  // ✅ TypeScript files
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/multiline-html-element-content-newline": "off",
+      "vue/html-indent": "off",
+      "vue/max-attributes-per-line": "off",
+    },
+  },
+  {
+    ignores: ["node_modules", "dist"],
+  },
+]
