a32dbbcb85dc4328c3e61367af5cd94236bca3ba
Adam Antal <adoantal@gmail.com>
2026-04-19 22:22:51 +0200

    Make TW custom classes work

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="flex h-screen items-center justify-center bg-gray-900">
-    <h1 class="text-4xl font-bold text-green-400">Pokémon Speed App ⚡</h1>
-    <p class="mt-4 text-white">Count: {{ count }}</p>
-    <button class="mt-4 rounded bg-blue-500 px-4 py-2 text-white" @click="increment">
-      Increment
-    </button>
-  </div>
+  <div class="bg-electric-blue h-screen w-screen">Content</div>
 </template>
 
 <script lang="ts">
